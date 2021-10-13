a3354fcf156340621c97b777ceb27d78c96950c5
fproko <fernandoprokopiuk@gmail.com>
2021-10-13 20:22:52 -0300

    Se agrega soporte de contador de ciclos

--- app/src/main.c
+++ app/src/main.c
@@ -1,15 +1,19 @@
 #include "board.h"
 #include "c_func.h"
 #include "asm_func.h"
+#include "cmsis.h"
 
 #include <stdlib.h>
 #include <stdnoreturn.h>
 
+#define ENABLE_DWT DWT->CTRL |= 1 << DWT_CTRL_CYCCNTENA_Pos
+#define RESET_CYCLES DWT->CYCCNT = 0
+#define GET_CYCLES DWT->CYCCNT
 
 // Variable que se incrementa cada vez que se llama al handler de interrupcion
 // del SYSTICK.
 static volatile uint32_t s_ticks = 0;
-
+volatile uint32_t ciclos = 0;
 
 // Inicia soporte de la placa y periodo de la interrupcion del SYSTICK
 // cada 1 milisegundo.
@@ -30,84 +34,139 @@ void SysTick_Handler (void)
 
 static void invertir(void)
 {
+    printf("\n\rEjercicio 9:\n\r");
     uint16_t vectorIn1[7] = {55,2,3,100,5,200,7};
     uint16_t vectorIn2[7] = {55,2,3,100,5,200,7};
+    
+    RESET_CYCLES;
     asm_invertir(vectorIn1,7);
+    printf("Invertir ASM : %d ciclos\n\r", GET_CYCLES);
+    
+    RESET_CYCLES;
     c_invertir(vectorIn2,7);
-
+    printf("Invertir C  : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void max(void)
 {
+    printf("\n\rEjercicio 7:\n\r");
     int32_t c_pos_max=0;
     int32_t asm_pos_max=0;
     uint32_t vectorIn1[8] = {-55,2,-3,100,-5,200,-7,8};
+    
+    RESET_CYCLES;
     asm_pos_max=asm_max(vectorIn1,8);
+    printf("max ASM : %d ciclos\n\r", GET_CYCLES);
+
+    RESET_CYCLES;
     c_pos_max=c_max(vectorIn1,8);
+    printf("max C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void pack32to16(void)
 {
+    printf("\n\rEjercicio 6:\n\r");
     uint32_t vectorIn1[8] = {-999999999,2,-3,4,-5,6,-7,8};
     uint16_t vectorOut1[8] = {0,0,0,0,0,0,0,0};
     uint16_t vectorOut2[8] = {0,0,0,0,0,0,0,0};
+
+    RESET_CYCLES;
     asm_pack32to16(vectorIn1,vectorOut1,8);
+    printf("pack32to16 ASM : %d ciclos\n\r", GET_CYCLES);
+
+    RESET_CYCLES;
     c_pack32to16(vectorIn1,vectorOut2,8);
+    printf("pack32to16 C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void filtroVentana10(void)
 {
+    printf("\n\rEjercicio 5:\n\r");
     uint16_t vectorIn1[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
     uint16_t vectorOut1[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     uint16_t vectorOut2[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    RESET_CYCLES;
     asm_filtroVentana10(vectorIn1, vectorOut1, 16);
+    printf("filtroVentana10 ASM : %d ciclos\n\r", GET_CYCLES);
+    
+    RESET_CYCLES;
     c_filtroVentana10(vectorIn1, vectorOut2, 16);
+    printf("filtroVentana10 C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void productoEscalar12(void)
 {
+    printf("\n\rEjercicio 4:\n\r");
     uint16_t vectorIn1[8] = {1,2,3,4,5,6,7,8};
     uint16_t vectorOut1[8] = {0,0,0,0,0,0,0,0};
     uint16_t vectorOut2[8] = {0,0,0,0,0,0,0,0};
     uint16_t vectorOut3[8] = {0,0,0,0,0,0,0,0};
 
+    RESET_CYCLES;
     asm_productoEscalar12(vectorIn1,vectorOut1,8,1000);
+    printf("asm_productoEscalar12 : %d ciclos\n\r", GET_CYCLES);
+
+    RESET_CYCLES;
     asm_productoEscalar12_sat(vectorIn1,vectorOut2,8,1000);
+    printf("asm_productoEscalar12_sat : %d ciclos\n\r", GET_CYCLES);
+    
+    RESET_CYCLES;
     c_productoEscalar12(vectorIn1,vectorOut2,8,1000);
+    printf("c_productoEscalar12 : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void productoEscalar16(void)
 {
+    printf("\n\rEjercicio 3:\n\r");
     uint16_t vectorIn1[8] = {1,2,3,4,5,6,7,8};
     uint16_t vectorOut1[8] = {0,0,0,0,0,0,0,0};
     uint16_t vectorIn2[8] = {1,2,3,4,5,6,7,8};
     uint16_t vectorOut2[8] = {0,0,0,0,0,0,0,0};
 
+    RESET_CYCLES;
     asm_productoEscalar16(vectorIn1,vectorOut1,8,3);
+    printf("productoEscalar12 ASM : %d ciclos\n\r", GET_CYCLES);
+    
+    RESET_CYCLES;
     c_productoEscalar16(vectorIn2,vectorOut2,8,3);
+    printf("productoEscalar12 C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void productoEscalar32(void)
 {
+    printf("\n\rEjercicio 2:\n\r");
     uint32_t vectorIn1[8] = {1,2,3,4,5,6,7,8};
     uint32_t vectorOut1[8] = {0,0,0,0,0,0,0,0};
     uint32_t vectorIn2[8] = {1,2,3,4,5,6,7,8};
     uint32_t vectorOut2[8] = {0,0,0,0,0,0,0,0};
 
+    RESET_CYCLES;
     asm_productoEscalar32(vectorIn1,vectorOut1,8,3);
+    printf("productoEscalar32 ASM : %d ciclos\n\r", GET_CYCLES);
+
+    RESET_CYCLES;
     c_productoEscalar32(vectorIn2,vectorOut2,8,3);
+    printf("productoEscalar32 C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void Zeros(void)
 {
+    printf("\n\rEjercicio 1:\n\r");
     uint32_t vector1[8] = {(u_int32_t)-1, (u_int32_t)-2, (u_int32_t)-3,
                            (u_int32_t)-4, (u_int32_t)-5, (u_int32_t)-6,
                            (u_int32_t)-7, (u_int32_t)-8};
     uint32_t vector2[8] = {(u_int32_t)-1, (u_int32_t)-2, (u_int32_t)-3,
                            (u_int32_t)-4, (u_int32_t)-5, (u_int32_t)-6,
                            (u_int32_t)-7, (u_int32_t)-8};
+    
+    RESET_CYCLES;
     asm_zeros(vector1,8);
+    printf("zeros ASM : %d ciclos\n\r", GET_CYCLES);
+
+    RESET_CYCLES;
     c_zeros(vector2,8);
+    printf("zeros C : %d ciclos\n\r", GET_CYCLES);
 }
 
 static void Suma (void)
@@ -225,21 +284,23 @@ noreturn void LoopInfinito (void)
 
 int main (void)
 {
+    ENABLE_DWT;
+
     Inicio ();
 
-    //Zeros(); //Ejercicio 1
+    Zeros(); //Ejercicio 1
 
-    //productoEscalar32(); //Ejercicio 2
+    productoEscalar32(); //Ejercicio 2
 
-    //productoEscalar16(); //Ejercicio 3
+    productoEscalar16(); //Ejercicio 3
 
-    //productoEscalar12(); //Ejercicio 4
+    productoEscalar12(); //Ejercicio 4
 
-    //filtroVentana10(); //Ejercicio 5
+    filtroVentana10(); //Ejercicio 5
 
-    //pack32to16(); //Ejercicio 6
+    pack32to16(); //Ejercicio 6
 
-    //max(); //Ejercicio 7
+    max(); //Ejercicio 7
 
     invertir(); //Ejercicio 9
 
